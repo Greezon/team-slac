aa31f6de4ec2363d31bbddc0e4792202f4334326
Ian A <iastesana@gmail.com>
2020-09-10 19:24:12 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require(“http”);
+const PORT = 3000;
+function handleRequest(request, response) {
+  response.end(“It Works!! Path Hit: ” + request.url);
+}
+const server = http.createServer(handleRequest);
+server.listen(PORT, function () {
+  console.log(“Server listening on: http://localhost:” + PORT);
+});
